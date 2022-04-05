d4ba652a25d8f2b5d89847436b86b3632d15670a
Sudo-Sam2017 <31296306+Sudo-Sam2017@users.noreply.github.com>
2022-04-04 22:52:43 -0400

    Counts total characters, number of vowels, and number of consanants.

--- vowelCounter.py
+++ vowelCounter.py
@@ -1,3 +1,22 @@
-with open('VowelTarget.txt') as f:
-    lines = f.readlines()
-    print(lines)
+file = open("VowelTarget.txt")
+
+line = file.read().replace("\n", " ")
+file.close()
+
+print(line)
+count = len(line)
+print('Number of Characters in this text file: ' + str(count))
+
+vowelList = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y']
+
+vowelCount = 0
+consanantCount = 0
+
+for element in line:
+    if(element in vowelList):
+        vowelCount = vowelCount + 1
+    else:
+        consanantCount = consanantCount + 1
+
+print("Vowels: " + str(vowelCount))
+print("Consanants: " + str(consanantCount))
